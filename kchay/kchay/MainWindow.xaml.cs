cf12856cd864e52df30781080afd4d80c98bd4a2
ikmsh <mrsopelka@gmail.com>
2024-10-24 15:22:32 +0500

    molniamakvin

---
+++ kchay/kchay/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace TextSizeApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = "Save Text File"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
