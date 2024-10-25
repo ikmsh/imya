7b5406fac56e89ae2cefd859f9beb2ecface3440
ikmsh <mrsopelka@gmail.com>
2024-10-25 10:46:51 +0500

    swag

---
+++ SWAAAG/SWAAAG/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ImageOpenDialogApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenImageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog { };
+
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif|All Files|*.*";
+            Title = "Open an Image File";
+            
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                img1.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+    }
+}
